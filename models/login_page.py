195a0749e2307fcdb7b23ca1215b0566cd1a63de
Lukas Trneny <trneny.lukas@email.cz>
2024-02-02 20:07:32 +0100

    added messages in login_page

--- models/login_page.py
+++ models/login_page.py
@@ -17,18 +17,22 @@ class LoginPage(BasePage):
     async def fill_username(self, text):
         expect(self.username).to_be_visible()
         await self.username.fill(text)
+        print("Username entered")
 
     async def fill_password(self, text):
         expect(self.password).to_be_visible()
         await self.password.fill(text)
+        print("Password entered")
 
     async def submit_continue(self):
         expect(self.submit_continue_button).to_be_visible()
         await self.submit_continue_button.click()
+        print("Clicked on continue button")
 
     async def submit_login(self):
         expect(self.submit_login_button).to_be_visible()
         await self.submit_login_button.click()
+        print("Clicked on login button")
 
     async def is_error_message_displayed(self):
         return await self.page.wait_for_selector(".error-text", state="visible", timeout=5000)
